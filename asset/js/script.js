9275acaf3ef713189aa3becd22ca37a9543706aa
almamun2s <mamun2social@gmail.com>
2024-06-11 01:01:37 +0600

    wow js added

--- asset/js/script.js
+++ asset/js/script.js
@@ -28,10 +28,32 @@ $(document).ready(function () {
 
 let contactBtn = document.querySelectorAll('.reach_btn')
 let popUp = document.getElementById('popup_contact');
-console.log(contactBtn);
 contactBtn.forEach(function (btn) {
     btn.addEventListener('click', function () {
         // alert('hi');
         popUp.classList.add('popup_contact_show');
     });
 });
+
+new WOW().init();
+
+function animateProgressBar() {
+    const progressBarAll = document.querySelectorAll('.progress-bar');
+    progressBarAll.forEach(progressBar => {
+
+        let targetValue = progressBar.getAttribute('aria-valuenow');
+        let currentValue = 0;
+        const interval = setInterval(() => {
+            if (currentValue >= targetValue) {
+                clearInterval(interval);
+            } else {
+                currentValue++;
+                progressBar.style.width = currentValue + '%';
+                progressBar.setAttribute('aria-valuenow', currentValue);
+            }
+        }, 20); // Adjust the interval for faster/slower animation
+    });
+}
+
+document.querySelector('.progress').addEventListener('animationend', animateProgressBar());
+
