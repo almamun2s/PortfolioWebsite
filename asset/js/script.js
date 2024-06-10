82284149d7d959b682ce2d72aedbcf178042b965
almamun2s <mamun2social@gmail.com>
2024-06-10 12:58:11 +0600

    About page added

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,24 +1,37 @@
+$(document).ready(function () {
 
-// Mobile menu
-$("#show_menu").click ( function(){
-    $(this).addClass("hide");
-    $("#hide_menu").removeClass("hide");
-    $(".main_menu ul").slideDown(500, function() {
-        $(this).css("display", "flex");
+    // Mobile menu
+    $("#show_menu").click(function () {
+        $(this).addClass("hide");
+        $("#hide_menu").removeClass("hide");
+        $(".main_menu ul").slideDown(500, function () {
+            $(this).css("display", "flex");
+        });
+
     });
-    
-});
-$("#hide_menu").click ( function(){
-    $(this).addClass("hide");
-    $("#show_menu").removeClass("hide");
-    $(".main_menu ul").slideUp(500);
+    $("#hide_menu").click(function () {
+        $(this).addClass("hide");
+        $("#show_menu").removeClass("hide");
+        $(".main_menu ul").slideUp(500);
+    });
+
+    // Contact Form 
+    $("#contact_btn").click(function () {
+        $("#popup_contact").addClass("popup_contact_show");
+    });;
+    $("#xmark").click(function () {
+        $("#popup_contact").removeClass("popup_contact_show");
+    });
+
 });
 
-// Contact Form 
-$("#contact_btn").click( function(){
-    $("#popup_contact").addClass("popup_contact_show");
-});;
-$("#xmark").click( function(){
-    $("#popup_contact").removeClass("popup_contact_show");
-});
 
+let contactBtn = document.querySelectorAll('.reach_btn')
+let popUp = document.getElementById('popup_contact');
+console.log(contactBtn);
+contactBtn.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+        // alert('hi');
+        popUp.classList.add('popup_contact_show');
+    });
+});
